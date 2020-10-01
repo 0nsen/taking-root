7d5ad5e6ca4a38c5d213fb075f8c5b28d80cb9e4
Phuong Gia <sphericalgia@gmail.com>
2020-10-01 22:40:46 +0700

    finished Item; minor fixes

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,47 +2,62 @@ import React from 'react'
 import './Item.css'
 import {aeonium, sedum, cactus, unusual} from '../Product-data/product-data'
 
-export default function Item(props) {
-    const productList = aeonium.concat(sedum, cactus, unusual);
+class Item extends React.Component {
+    constructor(props) {
+        super(props);
 
-    const itemName = (props.link).toUpperCase().replaceAll("-", " ");
+        const productList = aeonium.concat(sedum, cactus, unusual);
+        const itemName = (props.link).toUpperCase().replaceAll("-", " ");
+        this.product = productList.find(product => product.name === itemName);
 
-    const product = productList.find(product => product.name === itemName);
+        this.state = {
+            selected_price: this.product.prices[0]
+        }
 
-    return (
-        <section className="product-page">
-            <div className="product-image">
-                <img alt="plant" src={require("../Product-data/" + product.imageSrc)} />
-            </div>
-            <div className="product-info">
-                <h1 className="product-name">{product.name}</h1>
-                <h3>${product.prices[0]}</h3>
-                
-                <form className="sizes">
-                    <h5>SIZE</h5>
+        this.selectPrice = this.selectPrice.bind(this);
+    }
 
-                    <input name="size" id='2inch' type='radio' value='2 inch' defaultChecked/>
-                    <label for='2inch'>2 INCH</label>
-                    
-                    <input name="size" id='2inchpot' type='radio' value='2" + clay pot'/>
-                    <label for='2inchpot'>2" + CLAY POT</label>
-                    
-                    <input name="size" id='4inch' type='radio' value='4 inch'/>
-                    <label for='4inch'>4 INCH</label>
-                    
-                    <input name="size" id='4inchpot' type='radio' value='4" + clay pot'/>
-                    <label for='4inchpot'>4" + CLAY POT</label>
-                    
-                </form>
-                
-                <div className='quantity'>
-                    <h5>QUANTITY</h5>
-                    <input type="number" min="1" defaultValue="1"/>
+    selectPrice(e) {
+        this.setState({
+            selected_price: this.product.prices[this.product.sizes.indexOf(e.target.value)]
+        });
+    }
+    
+    render() {
+        return (
+            <section className="product-page">
+                <div className="product-image">
+                    <img alt="plant" src={require("../Product-data/" + this.product.imageSrc)} />
                 </div>
                 
-                <button className="add-to-cart">ADD TO CART</button>
-            </div>
-        </section>
-    );
+                <div className="product-info">
+                    <h1 className="product-name">{this.product.name}</h1>
+                    <h3>${this.state.selected_price}</h3>
+                    
+                    <form className="sizes">
+                        <h5>SIZE</h5>
+                        <div className="sizes__buttons">
+                            {this.product.sizes.map(size => {
+                                return (
+                                    <div>
+                                        <input onClick={this.selectPrice} name="size" id={size.replaceAll(" ", "-")} type="radio" value={size}/>
+                                        <label for={size.replaceAll(" ", "-")}>{size}</label>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </form>
+                    
+                    <div className='quantity'>
+                        <h5>QUANTITY</h5>
+                        <input type="number" min="1" defaultValue="1"/>
+                    </div>
+                    
+                    <button className="add-to-cart">ADD TO CART</button>
+                </div>
+            </section>
+        );
+    }
 }
 
+export default Item;
